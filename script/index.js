7ffa35f01fff414ce6e5db02abe834b016e890ab
Johirul Islam Himel <himel1199@gmail.com>
2026-03-04 14:41:00 +0600

    Speak your vocabulary

--- script/index.js
+++ script/index.js
@@ -3,6 +3,12 @@ const createElements = (arr) => {
   return htmlElements.join(" ");
 };
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 const manageSpinner = (status) => {
   if (status == true) {
     document.getElementById("spinner").classList.remove("hidden");
@@ -138,7 +144,7 @@ const displayLevelWord = (words) => {
           <button onclick = "loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] hover:text-white border-none">
             <i class="fa-solid fa-circle-info"></i>
           </button>
-          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] hover:text-white border-none">
+          <button onclick = "pronounceWord('${word.word}')"  class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] hover:text-white border-none">
             <i class="fa-solid fa-volume-high"></i>
           </button>
         </div>
